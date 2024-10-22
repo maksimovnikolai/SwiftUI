39f4e094638d2e50b7c04335ee45cf960fb2f6b3
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-10-22 18:23:13 +0300

    SCF193-MemorizeApp-Lecture 1

---
+++ Stanford CS193/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nikolai Maksimov on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
